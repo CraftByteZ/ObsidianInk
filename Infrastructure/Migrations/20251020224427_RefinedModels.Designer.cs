3271cec488e75c3f7505acad026a925508f4f0dd
Zelidee <zelideeguemez@gmail.com>
2025-10-20 18:46:01 -0400

    Database Update

--- Infrastructure/Migrations/20251020224427_RefinedModels.Designer.cs
+++ Infrastructure/Migrations/20251020224427_RefinedModels.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ObsidianInk.Data;
@@ -11,9 +12,11 @@ using ObsidianInk.Data;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(ObsidianInkContext))]
-    partial class ObsidianInkContextModelSnapshot : ModelSnapshot
+    [Migration("20251020224427_RefinedModels")]
+    partial class RefinedModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Infrastructure/Migrations/20251020224427_RefinedModels.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefinedModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "Reviews",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "Reviews",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000);
+        }
+    }
+}
